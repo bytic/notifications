40857d60464e8af1058160e0e48443de559ebd45
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-06-15 15:06:35 +0300

    initial update to new notifications

---
+++ src/Exceptions/NotificationRecipientModelNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ByTIC\Notifications\Exceptions;
+
+/**
+ * Class NotificationRecipientModelNotFoundException
+ * @package ByTIC\Notifications\Exceptions
+ */
+class NotificationRecipientModelNotFoundException extends \Exception
+{
+}
