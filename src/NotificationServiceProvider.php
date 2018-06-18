eff2e315aa460d41ceab2e0d28aacca4c00bca22
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-06-18 13:26:56 +0300

    fix service provider to 1.0

--- src/NotificationServiceProvider.php
+++ src/NotificationServiceProvider.php
@@ -5,13 +5,13 @@ namespace ByTIC\Notifications;
 use ByTIC\Notifications\Channels\ChannelInterface;
 use ByTIC\Notifications\Dispatcher\Dispatcher;
 use ByTIC\Notifications\Dispatcher\DispatcherInterface;
-use Nip\Container\ServiceProvider\AbstractSignatureServiceProvider;
+use Nip\Container\ServiceProviders\Providers\AbstractServiceProvider;
 
 /**
  * Class NotificationServiceProvider
  * @package ByTIC\Notifications
  */
-class NotificationServiceProvider extends AbstractSignatureServiceProvider
+class NotificationServiceProvider extends AbstractServiceProvider
 {
 
     /**
@@ -26,13 +26,13 @@ class NotificationServiceProvider extends AbstractSignatureServiceProvider
     protected function registerChannels()
     {
         $manager = new ChannelManager();
-        $this->getContainer()->singleton(ChannelInterface::class, $manager);
+        $this->getContainer()->share(ChannelInterface::class, $manager);
     }
 
     protected function registerDispatcher()
     {
         $dispatcher = new Dispatcher(app(ChannelInterface::class));
-        $this->getContainer()->singleton(DispatcherInterface::class, $dispatcher);
+        $this->getContainer()->share(DispatcherInterface::class, $dispatcher);
     }
 
     /**
